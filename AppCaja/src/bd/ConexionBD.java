ad80436ea274b71c3707fcc517dc2e511698b725
Geraldo Achuy <geraldo62012@gmail.com>
2015-12-11 15:37:32 -0500

    Viernes 11 12 15 3:36 pm
    Creación de proyecto : Sistema de Caja
    Lenguaje : Java
    BD: Mysql

---
+++ AppCaja/src/bd/ConexionBD.java
@@ -0,0 +1,17 @@
+package bd;
+
+
+//Clase para conectar con la base de datos Mysql
+public class ConexionBD {
+    
+    public static String bd="";
+    public static String puerto="3306";
+    public static String host="localhost";
+    public static String url="";
+    public static String usuario="root";
+    public static String password="admin";
+    
+    
+    
+    
+}
